903bc8e5c062c2e024e7d6efb7d5626ba49d36b3
unknown <the0ther@.(none)>
2011-01-30 17:07:37 -0500

    changed case of directory names

--- databases/lessons/2011-2-2/setup.sh
+++ databases/lessons/2011-2-2/setup.sh
@@ -11,10 +11,10 @@ wget http://downloads.mysql.com/docs/sakila-db.zip;
 wget http://downloads.mysql.com/docs/menagerie-db.zip;
 
 # make dir structure
-mkdir --parents ~/Databases/Lessons/2011-2-2/Menagerie;
+mkdir --parents ~/databases/lessons/2011-2-2/menagerie;
 
 # unzip menagerie
-unzip menagerie-db.zip -d ~/Databases/Lessons/2011-2-2/Menagerie;
+unzip menagerie-db.zip -d ~/Databases/Lessons/2011-2-2/menagerie;
 
 # put the zip in the trash
 mv menagerie-db.zip ~/.Trash;
