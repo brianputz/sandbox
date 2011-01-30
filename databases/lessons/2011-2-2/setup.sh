f1b2d2ae7f4c2b4764b8963dd6bde2b5a19efe80
unknown <the0ther@.(none)>
2011-01-30 17:15:45 -0500

    had path trouble

--- databases/lessons/2011-2-2/setup.sh
+++ databases/lessons/2011-2-2/setup.sh
@@ -2,10 +2,13 @@
 mkdir ~/Trash;
 
 # download world database zip file
-wget http://downloads.mysql.com/docs/world_innodb.sql.zip;
+# wget http://downloads.mysql.com/docs/world_innodb.sql.zip;
 
 # download sakila database zip
-wget http://downloads.mysql.com/docs/sakila-db.zip;
+# wget http://downloads.mysql.com/docs/sakila-db.zip;
+
+# move to home directory
+cd ~;
 
 # download menagerie database zip
 wget http://downloads.mysql.com/docs/menagerie-db.zip;
@@ -14,7 +17,7 @@ wget http://downloads.mysql.com/docs/menagerie-db.zip;
 mkdir --parents ~/databases/lessons/2011-2-2/menagerie;
 
 # unzip menagerie
-unzip menagerie-db.zip -d ~/Databases/Lessons/2011-2-2/menagerie;
+unzip ~/menagerie-db.zip -d ~/Databases/Lessons/2011-2-2/menagerie;
 
 # put the zip in the trash
-mv menagerie-db.zip ~/.Trash;
+mv ~/menagerie-db.zip ~/.Trash;
