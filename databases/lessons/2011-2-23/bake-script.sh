9bed6544d2b4b613a66846a69324b4b62ddaa019
randy <the0ther@mini10.(none)>
2011-02-23 00:11:52 -0500

    adding in a script to magically roll out a cake install for cake-baking

---
+++ databases/lessons/2011-2-23/bake-script.sh
@@ -0,0 +1,7 @@
+ROOT=/tmp;
+echo $ROOT;
+mkdir --parents ~/public_html/databases/lessons/2011-2-23$ROOT/baked-blog;
+cd ~/public_html/databases/lessons/2011-2-23$ROOT/baked-blog;
+wget http://a.parsons.edu/~loffelmr/cakephp.tar.gz;
+tar -xzf cakephp.tar.gz;
+
