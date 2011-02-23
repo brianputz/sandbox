cee10cf3c106fee4b01b0a28e07d1ff9d9fa717d
randy <the0ther@mini10.(none)>
2011-02-22 23:04:42 -0500

    adding in a simple SQL injection demo

---
+++ databases/lessons/2011-2-23/inject.php
@@ -0,0 +1,20 @@
+<?php
+function login(user,pwd) {
+	//lookup the username & password in database, return  true for success
+	
+}
+?><!DOCTYPE html>
+<html>
+<head>
+	<title>SQL Injection</title>
+</head>
+<body>
+	<form>
+	<fieldset>
+		<label>Username</label> <input type="text" id="username" /><br/>
+		<label>Password</label> <input type="password" id="password" /><br/>
+		<input type="submit" id="submit1" value="Login" />
+	</fieldset>
+	</form>
+</body>
+</html>
