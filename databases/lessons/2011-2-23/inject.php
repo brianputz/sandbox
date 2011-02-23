dceb07af61be8d6503a2c9bcbfe5b0d9de920552
randy <the0ther@mini10.(none)>
2011-02-22 23:51:58 -0500

    working on sql injection demo

--- databases/lessons/2011-2-23/inject.php
+++ databases/lessons/2011-2-23/inject.php
@@ -1,7 +1,23 @@
 <?php
-function login(user,pwd) {
+function login($user,$pwd) {
 	//lookup the username & password in database, return  true for success
-	
+	$link = mysql_connect('localhost', 'loffelmr', '0r^n63') 
+					or die('Could not connect: ' . mysql_error());
+	mysql_select_db('loffelmr') or die('Could not select database');
+	$query = "SELECT * FROM mydb_users WHERE username='$user' AND password='$pwd';";
+	$results = mysql_fetch_assoc($query) or die('qry failed: '.mysql_error());
+	if (mysql_num_rows()>=1) {
+		return true;
+	} else {
+		return false;
+	}
+}
+// if we get form data try to login
+$isLoggedIn = false;
+if (isset($_GET['submit1'])) {
+	$uu = $_GET['username'];
+	$pp = $_GET['password']; 
+	$isLoggedIn = login($uu,$pp);
 }
 ?><!DOCTYPE html>
 <html>
@@ -9,6 +25,13 @@ function login(user,pwd) {
 	<title>SQL Injection</title>
 </head>
 <body>
+<?
+if ($isLoggedIn) {
+	echo '<h1>You are logged in</h1>';
+} else {
+	echo '<h1>Please login</h1>';
+}
+?>
 	<form>
 	<fieldset>
 		<label>Username</label> <input type="text" id="username" /><br/>
