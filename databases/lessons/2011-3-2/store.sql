d86626d34c75f16e318ed4df16fc335ff83c8eb4
unknown <the0ther@.(none)>
2011-02-26 21:37:46 -0500

    more DB work

--- databases/lessons/2011-3-2/store.sql
+++ databases/lessons/2011-3-2/store.sql
@@ -6,24 +6,31 @@ DROP TABLE IF EXISTS store_sizes;
 
 /* BOOTS */
 CREATE TABLE store_boots (
+	id 			INT UNSIGNED NOT NULL AUTO_INCREMENT KEY,
 	name 		VARCHAR(50),
 	sku			INT,
 	height		VARCHAR(15),
 	lining		VARCHAR(15),
 	colors		VARCHAR(255),
 	price		DECIMAL(7,2), /* 7 digits, maximum of 2 digits past decimal point */
-	features	VARCHAR(255),
-	id 			INT UNSIGNED NOT NULL AUTO_INCREMENT KEY
+	features	VARCHAR(255)
 );
 
 CREATE TABLE store_colors (
+	id 		INT UNSIGNED NOT NULL AUTO_INCREMENT KEY,
 	name 	VARCHAR(50),
-	id 		INT UNSIGNED NOT NULL AUTO_INCREMENT KEY	
+	value	VARCHAR(2)
+);
+
+CREATE TABLE store_boots_sizes (
+	id 			INT UNSIGNED NOT NULL AUTO_INCREMENT KEY,
+	boot_id		INT,
+	size_id		INT
 );
 
 CREATE TABLE store_sizes (
-	name 	VARCHAR(50),
-	id 		INT UNSIGNED NOT NULL AUTO_INCREMENT KEY
+	id 		INT UNSIGNED NOT NULL AUTO_INCREMENT KEY,
+	name 	VARCHAR(50)
 );
 
 INSERT INTO store_boots (id,name)
@@ -37,8 +44,39 @@ VALUES
 ('7141730', 'Danner Foreman Pro Work Boot'),
 ('7257914', 'Danner Grouse GTX Boot');
 
-/*
-INSERT INTO store_colors (id,name)
+INSERT INTO store_colors (id,name,value)
 VALUES
-()
+(1,'Black Oil-tanned','bk'),
+(2,'Black Polishable','br'),
+(3,'Black','bk'),
+(4,'Crazy Horse','br'),
+(5,'Honey','hy'),
+(6,'Peanut','sd'),
+(7,'Brown','rw'),
+(8,'Russet','br'),
+(9,'Brown','br');
+
+INSERT INTO store_sizes (id, name, value)
+VALUES
+(1,'9d','9 D'),
+(2,'9ee','9 EE'),
+(3,'95d','9\u00BD D'),
+(4,'95ee','9\u00BD EE'),
+(5,'95eee','9\u00BD EEE'),
+(6,'10ee','10 EE'),
+(7,'10eee','10 EEE'),
+(8,'13e','13 E');
+
+/*
+INSERT INTO store_boots_sizes (boot_id,size_id)
+VALUES
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+();
 */
