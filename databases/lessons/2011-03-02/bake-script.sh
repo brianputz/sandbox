54b435ce1456e7d414f3750a7ad042c815f04eaa
randy <the0ther@mini10.(none)>
2011-03-02 02:12:13 -0500

    small change

--- databases/lessons/2011-03-02/bake-script.sh
+++ databases/lessons/2011-03-02/bake-script.sh
@@ -1,6 +1,6 @@
 ROOT=/tmp;
 echo "Building a bake-blog in $ROOT";
-mkdir --parents ~/public_html/databases/lessons/2011-03-02$ROOT/baked-blog;
-cd ~/public_html/databases/lessons/2011-03-02$ROOT/baked-blog;
-wget --recursive http://a.parsons.edu/~loffelmr/cake-for-class.tar.gz;
+mkdir --parents ~/public_html/databases/2011-03-02$ROOT/baked-blog;
+cd ~/public_html/databases/2011-03-02$ROOT/baked-blog;
+`wget http://a.parsons.edu/~loffelmr/cake-for-class.tar.gz`;
 tar xzf cake-for-class.tar.gz;
