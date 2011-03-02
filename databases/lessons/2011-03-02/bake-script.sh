ca4bfa51d334f362ecfff0668e6aa10b576f3060
randy <the0ther@mini10.(none)>
2011-03-02 02:49:30 -0500

    final bash script

--- databases/lessons/2011-03-02/bake-script.sh
+++ databases/lessons/2011-03-02/bake-script.sh
@@ -1,6 +1,10 @@
-ROOT=/tmp;
 echo "Building a bake-blog in $ROOT";
-mkdir --parents ~/public_html/databases/2011-03-02$ROOT/baked-blog;
-cd ~/public_html/databases/2011-03-02$ROOT/baked-blog;
-`wget http://a.parsons.edu/~loffelmr/cake-for-class.tar.gz`;
-tar xzf cake-for-class.tar.gz;
+mkdir --parents ~/public_html/databases/2011-03-02;
+cd ~/public_html/databases/2011-03-02;
+wget 'http://a.parsons.edu/~loffelmr/blog.tar.gz';
+tar xzf blog.tar.gz;
+rm blog.tar.gz;
+chmod -R 775 blog/;
+#NOW RUN THE MYSQL
+wget 'http://a.parsons.edu/~loffelmr/blog2.sql';
+mysql -u loffelmr -p loffelmr < blog2.sql;
