866a33e60fd6b6457173fca47797be4cf5f8514d
unknown <the0ther@.(none)>
2011-03-02 00:26:02 -0500

    working on the jquery tut

--- databases/lessons/2011-03-02/jquery/starterkit/custom.js
+++ databases/lessons/2011-03-02/jquery/starterkit/custom.js
@@ -1,3 +1,24 @@
-jQuery(document).ready(function() {
-	// do something here
-});
+$(document).ready(function() {
+   // generate markup
+   $("#rating").append("Please rate: ");
+   
+   for ( var i = 1; i <= 5; i++ )
+     $("#rating").append("<a href='#'>" + i + "</a> ");
+   
+   // add markup to container and apply click handlers to anchors
+   $("#rating a").click(function(e){
+     // stop normal link click
+     e.preventDefault();
+     
+     // send request
+     $.post("http://sdcweb1.org/rate.php", {rating: $(this).html()}, function(xml) {
+       // format and output result
+       $("#rating").html(
+         "Thanks for rating, current average: " +
+         $("average", xml).text() +
+         ", number of votes: " +
+         $("count", xml).text()
+       );
+     });
+   });
+ });
