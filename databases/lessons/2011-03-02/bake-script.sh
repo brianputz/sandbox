e8e5f7ef9aba2e42921578599a63a2d3c8df05c6
randy <the0ther@mini10.(none)>
2011-03-02 02:07:37 -0500

    updating shell script

--- databases/lessons/2011-03-02/bake-script.sh
+++ databases/lessons/2011-03-02/bake-script.sh
@@ -1,5 +1,6 @@
 ROOT=/tmp;
 echo "Building a bake-blog in $ROOT";
-mkdir --parents ~/public_html/databases/lessons/2011-3-2$ROOT/baked-blog;
-cd ~/public_html/databases/lessons/2011-2-23$ROOT/baked-blog;
-wget --recursive http://a.parsons.edu/~loffelmr/sandbox/databases/temp/cake-for-class/*;
+mkdir --parents ~/public_html/databases/lessons/2011-03-02$ROOT/baked-blog;
+cd ~/public_html/databases/lessons/2011-03-02$ROOT/baked-blog;
+wget --recursive http://a.parsons.edu/~loffelmr/cake-for-class.tar.gz;
+tar xzf cake-for-class.tar.gz;
